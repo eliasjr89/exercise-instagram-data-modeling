4fc4583bd1d12a25ea0dbd55539529d6ce575caa
Elías JR <167987056+eliasjr89@users.noreply.github.com>
2024-09-05 13:13:01 +0000

    changes

--- src/models.py
+++ src/models.py
@@ -55,4 +55,9 @@ class Follower(Base):
 
 
 
-render_er(Base, "diagram.png")
+try:
+    result = render_er(Base, 'diagram.png')
+    print("Success! Check the diagram.png file")
+except Exception as e:
+    print("There was a problem genering the diagram")
+    raise e
